68a78e05d05c5ced1909002a138ce2ce315a7f54
Isaac Rios <jirb1213@live.com.mx>
2025-09-28 20:09:43 -0600

    Base components: Card and cardGrid

---
+++ js/components/Card.js
@@ -0,0 +1,46 @@
+import { displayIdNumber } from '../utils/utils.js'
+import Image from './ImageContainer.js'
+import { PokemonTypes } from './Tag.js'
+
+/**
+ * Creates a card element representing a Pokemon
+ * 
+ * The card includes :
+ * A sprite image
+ * the pokemon id
+ * the pokemon name
+ * the pokemon types
+ * @param {Object} data - The pokemon data
+ * @param {string} data.id - Unique id for the pokemon
+ * @param {string} data.name - Display the pokemon name
+ * @param {string[]} data.types - Array of pokemon types
+ * @param {string} data.sprite - The URL of the pokemon sprite image
+ * @returns {HTMLDivElement} - representing the pokemon
+ */
+const Card=({data})=>{
+    const {id, name,types,sprite } = data
+    const container = document.createElement('div')
+    container.classList.add('card')
+
+    const imageContainer = Image({className:'card-image',sprite})
+
+    const pokemonNameAndId = document.createElement('div')
+    pokemonNameAndId.classList.add('card-name-id')
+
+    const pokemonName = document.createElement('h3')
+    pokemonName.textContent=name
+    
+
+    const pokemonNumber = document.createElement('span')
+    pokemonNumber.textContent=`#${displayIdNumber(id)}`
+
+    pokemonNameAndId.append(pokemonNumber,pokemonName)
+
+    const pokemonType = PokemonTypes(types)
+
+    container.append(imageContainer,pokemonNameAndId,pokemonType)
+
+    return container
+}
+
+export default Card
---
+++ js/components/CardsGrid.js
@@ -0,0 +1,72 @@
+import {team$,selectedPokemon$} from '../utils/observer.js'
+import Card from './Card.js'
+
+/** TODO: 
+ * - Add smooth animations for the creation of each card
+ * - Add loaders to enhance UX
+*/
+
+/**Creates a responsive grid container for displaying the Pokemon team
+ * 
+ * The grid stays in sync with the 'team$' observable:
+ * - On updates, replaces the current element with the new team
+ * - If the team is empty, it shows a fallback message
+ * 
+ * Clicking on a card will notify `selectedPokemon$` with the pokemon ID
+ * 
+ * @returns {HTMLDivElement} a <div> element with the `cards-grid` class
+ */
+export const CardsGrid=()=>{
+    const container = document.createElement('div')
+    container.classList.add('cards-grid')
+
+    let prevTeam = []
+
+    /**
+     * Renders the current team into the grid
+     * Replaces all existing children to ensure consistency
+     * Falls back to a message if no team is available
+     * 
+     * @param {Array<Object>} newTeam  - The new team to render 
+     */
+    const render = (newTeam =[])=>{
+        container.replaceChildren()
+        if(!Array.isArray(newTeam)|| newTeam.length===0){
+            container.replaceChildren()
+            const emptyMsg= document.createElement('p')
+            emptyMsg.textContent='Click "Generate team" to display a team'
+            container.appendChild(emptyMsg)
+            prevTeam=[]
+            return
+        }
+
+        // Render each Pokemon card
+        newTeam.forEach(newPokemon =>{
+            const newCard = Card({data:newPokemon})
+            newCard.dataset.id=newPokemon.id
+            container.appendChild(newCard)
+        })
+
+        // keep a copy of the current team for reference
+        prevTeam= newTeam.slice() 
+    }
+
+    // Event Delegation:
+    // Listens for click on any card inside the grid,
+    // then notifies `selectedPokemon$` with the clicked Pokemon id
+    container.addEventListener('click',e=>{
+        const card = e.target.closest('.card')
+        if(card && container.contains(card)){
+            const pokemonId = card.dataset.id
+            selectedPokemon$.notify(pokemonId)
+        }
+    })
+
+    // The initial render (in case observable already has a team)
+    render(team$.getValue())
+
+    // Subscribe for updates => every button click triggers re-render
+    team$.subscribe(team=>render(team))
+
+    return container
+}
