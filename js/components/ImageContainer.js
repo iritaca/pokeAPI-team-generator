52a07c38bab08830f456bee18642f00b3bd8a4e0
Isaac Rios <jirb1213@live.com.mx>
2025-10-10 16:59:47 -0600

    Image component enhanced by adding alt for accessibility

--- js/components/ImageContainer.js
+++ js/components/ImageContainer.js
@@ -2,14 +2,18 @@
  * Creates a semantic image container (<figure> with <img>)
  * 
  * @param {string} sprite - the image source to be shown
+ * @param {string} imgAlt - A string for <img alt=''/> attribute
  * @param {string} [className] - Optional additional CSS class for custom styling
  * @returns {HTMLElement} a <figure> element containing the <img>
  */
-const Image=({className,sprite})=>{
+const Image=({className,sprite,imgAlt})=>{
     const imageEl = document.createElement('figure')
     imageEl.classList.add(className)
+    
     const image = document.createElement('img')
     image.src=sprite
+    image.alt=imgAlt
+    
     imageEl.appendChild(image)
     return imageEl
 }
